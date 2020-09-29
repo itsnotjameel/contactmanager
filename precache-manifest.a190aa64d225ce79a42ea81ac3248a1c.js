e82911c27b91f4fe7cc76054b3d927bdfbd5e661
Jameel Amir <jameelozbiz@gmail.com>
2020-09-29 16:20:22 +0300

    Updates

---
+++ precache-manifest.a190aa64d225ce79a42ea81ac3248a1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362bf21f14cf298d2010f2b1b3f4098e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "9df3173c539022f40f38",
+    "url": "/contactmanager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bc633a550e141efe4b8d",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9df3173c539022f40f38",
+    "url": "/contactmanager/static/js/2.10a3ff1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.10a3ff1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc633a550e141efe4b8d",
+    "url": "/contactmanager/static/js/main.90da5ceb.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
