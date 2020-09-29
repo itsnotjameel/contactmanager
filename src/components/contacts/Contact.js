5f08bef8d0f5caaca648b517a0760a3db88de2d7
Jameel Amir <jameelozbiz@gmail.com>
2020-09-29 16:04:47 +0300

    removed popup test

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -7,7 +7,6 @@ import axios from "axios";
 class Contact extends Component {
   state = {
     showContactInfo: false,
-    popupIsOpen: false,
   };
 
   onDeleteClick = async (id, dispatch) => {
@@ -22,8 +21,6 @@ class Contact extends Component {
   render() {
     const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
-    const toggleIsOpen = () =>
-      this.setState({ popupIsOpen: !this.state.popupIsOpen });
     return (
       <Consumer>
         {(value) => {
@@ -46,41 +43,19 @@ class Contact extends Component {
                   style={{ cursor: "pointer", float: "right", color: "red" }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 ></i>
-                {/* <Link to={`contact/edit/${id}`}> */}
-                <i
-                  className="fas fa-user-edit"
-                  style={{
-                    cursor: "pointer",
-                    float: "right",
-                    marginRight: "1rem",
-                    color: "black",
-                  }}
-                  onClick={toggleIsOpen}
-                ></i>
-                {/* </Link> */}
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-user-edit"
+                    style={{
+                      cursor: "pointer",
+                      float: "right",
+                      marginRight: "1rem",
+                      color: "black",
+                    }}
+                    onClick={toggleIsOpen}
+                  ></i>
+                </Link>
               </h4>
-              {this.state.popupIsOpen && (
-                <Popup
-                  content={
-                    <>
-                      <b>Design your Popup</b>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                      </p>
-                      <button>Test button</button>
-                    </>
-                  }
-                  handleClose={toggleIsOpen}
-                />
-              )}
               {showContactInfo ? (
                 <ul className="list-group">
                   <li className="list-group-item">Email: {email}</li>
